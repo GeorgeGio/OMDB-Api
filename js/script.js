319c4f6fb91d61fb224daff5ba4b858b485a5d41
George <georgeoustapas@gmail.com>
2023-02-07 20:55:08 +0000

    added event listener and created class attribute for similar movies button

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@ let userMovie = "";
 let userMoviePosterUrl = "";
 let similarMoviesArr = [];
 let similarMoviesToShow = 3;
-let similarMovie1 = document.querySelector("#similarMovie1");
-let similarMovie2 = document.querySelector("#similarMovie2");
-let similarMovie3 = document.querySelector("#similarMovie3")
+// let similarMovie1 = document.querySelector("#similarMovie1");
+// let similarMovie2 = document.querySelector("#similarMovie2");
+// let similarMovie3 = document.querySelector("#similarMovie3");
 
 let movieSearchEl = document.querySelector("#movie-search");
 let moviePosterEl = document.querySelector("#movie-poster");
+let similarButtonEl = document.querySelector(".simButMov");
 
 let movieSearchButtonEl = document.querySelector("#movie-search-button");
 
@@ -21,59 +22,72 @@ movieSearchButtonEl.addEventListener("click", function () {
 
   console.log("userMovie", userMovie);
 
- 
+
   displayMoviePoster(userMovie);
 
 
   getSimilarMovies(userMovie);
 
 
-  
+
 });
+
 
 
 
 function displayMoviePoster(movie) {
   fetch(`https://www.omdbapi.com/?t=${movie}&apikey=b8054373`)
-    .then((response) => response.json())
-    //   .then(data => console.log(data));
-    .then((data) => {
-      //could condense these 2 lines into 1
-      userMoviePosterUrl = data.Poster;
+  .then((response) => response.json())
+  //   .then(data => console.log(data));
+  .then((data) => {
+    //could condense these 2 lines into 1
+    userMoviePosterUrl = data.Poster;
+    
+    moviePosterEl.src = userMoviePosterUrl;
+    
+    let similarButton = document.body.appendChild(document.createElement("button"));
+    console.log(similarButton);
+    similarButton.textContent = "Similar movies";
+    similarButton.setAttribute("class" , "simButMov");
+    let buttonClass = document.querySelector(".simButMov");
 
-      moviePosterEl.src = userMoviePosterUrl;
+    
+    buttonClass.addEventListener("click",function(){
+      console.log("works");
+      let similarMoviesString = localStorage.getItem("SimilarMovies");
+      
+      // console.log(similarMoviesArr);
+      similarMoviesArr = JSON.parse(similarMoviesString) || [];
+      // console.log(similarMoviesArr);
 
-      let similarButton = document.body.appendChild(document.createElement("button"));
-      similarButton.textContent = "Similar movies";
-
-      // getSimilarMovies(movie);
     });
+    
+    // getSimilarMovies(movie);
+  });
 }
 
-function getSimilarMovies(movie){
-  
+function getSimilarMovies(movie) {
 
 
 
- // fetch("https://tastedive.com/api/similar?q=movie:matrix&k=447474-Project1-74HRK333")
- fetch("https://tastedive.com/api/similar?q=movie:"+ movie +"&k=447625-Project1-NTMYY5L9&limit=3")
-  .then(response => response.json())
-  // .then(similarMovies => console.log(similarMovies))
-  .then((similarMovies) => {
-    
-    for (let index = 0; index < 3; index++) {
-      let movieIndex = similarMovies.Similar.Results[index].Name;
-      similarMoviesArr.push(movieIndex);
-      
-      
-    }
-    localStorage.setItem("SimilarMovies", similarMoviesArr);
-    
-  
-  })
-  
-  
+
+  // fetch("https://tastedive.com/api/similar?q=movie:matrix&k=447474-Project1-74HRK333")
+  fetch("https://tastedive.com/api/similar?q=movie:" + movie + "&k=447625-Project1-NTMYY5L9&limit=3")
+    .then(response => response.json())
+    // .then(similarMovies => console.log(similarMovies))
+    .then((similarMovies) => {
+      let localArray = [];
+      for (let index = 0; index < 3; index++) {
+        let movieIndex = similarMovies.Similar.Results[index].Name;
+        localArray.push(movieIndex);
+
+
+      }
+      localStorage.setItem("SimilarMovies",JSON.stringify(localArray));
+
+
+    })
 
 }
-  
+
 
