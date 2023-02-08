7406bc2c9e151ee8603ee6cd62b61202e0d2f09d
manolisgi <114882721+manolisgi@users.noreply.github.com>
2023-02-08 19:30:12 +0000

    added path rule to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+
+  content: ["./src/**/*.{html,js}"],
+
   theme: {
+
     extend: {},
+
   },
+
   plugins: [],
+
 }
